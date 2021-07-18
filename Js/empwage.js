548d1337ade4c300aed9c396d8e87192d3f2b88a
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-07-18 22:50:10 +0530

    [akhila]refactor:Adding evnt listener to name and date

--- Js/empwage.js
+++ Js/empwage.js
@@ -1,10 +1,28 @@
-// UC8-Adding Event listnet to salary to set the range
-var salary = document.querySelector("#salary");
-var output=  document.querySelector(".salary-output");
+//Adding Event listener to name and date 
+window.addEventListener('DOMContentLoaded',(event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input',function() {
+        if(name.value.length == 0) {
+            textError.textContent="";
+            return;
+        }
+        try{
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent = "";
+        }
+        catch(e){
+            textError.textContent = e;
+        }
+    }); 
+// Adding Event listnet to salary to set the range
+const salary = document.querySelector("#salary");
+const output=  document.querySelector(".salary-output");
 output.textContent = salary.value;
 salary.addEventListener('input',function() {
 output.textContent = salary.value;
- })
+ });
+});
 //UC9-Adding getter setter method and validation to emppayroll app
 class EmployeePayrollData{
     //getter and setter method
