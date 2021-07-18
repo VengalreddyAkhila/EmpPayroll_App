fb621516f11c2c44d749d50a5515b921c032efde
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-07-18 21:51:46 +0530

    [akhila]Refactor:Adding event listener to salary

--- Js/empwage.js
+++ Js/empwage.js
@@ -1,4 +1,4 @@
-
+// UC8-Adding Event listnet to salary to set the range
 var salary = document.querySelector("#salary");
 var output=  document.querySelector(".salary-output");
 output.textContent = salary.value;
@@ -6,3 +6,34 @@ salary.addEventListener('input',function() {
 output.textContent = salary.value;
  })
 
+class EmployeePayrollData{
+    //property
+    id;
+    salary;
+    gender;
+    startdate;
+    //constructor
+   constructor(...params){
+       this.id = params[0];
+       this.name = params[1];
+       this.salary = params[2];
+       this.gender = params[3];
+       this.startdate = params[4];
+   }
+   //getter setter method
+   get name() { return this._name;}
+   set name(name) {
+       let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+       if(nameRegex.test(name))
+       this._name = name;
+       else 
+       throw 'Name is incorrect!';
+   }
+   //method
+   toString()
+   const options = {year: 'numeric', month:'long' ,day='numeric' };
+   const empDate = !this.startDate ? "undefined" :
+                      this.startDate.toLocalDateString("en-US",options);
+    return "id=" +this.id+ ", name=" +this.name + ", salary= " + this.salary +","+
+         "gender=" +this.gender + ",startDate=" +empDate;
+}
