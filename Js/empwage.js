ad55ad8265d8d1ed76a5089f85db4237b6681139
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-07-18 18:07:53 +0530

    [akhila]Refactor:specifying the date month year in form

--- Js/empwage.js
+++ Js/empwage.js
@@ -1,9 +1,8 @@
-<script>
-const salary = document.querySelector('#salary');
-                const output=  document.querySelector('.salary-output');
-                output.textContent = salary.value;
-                salary.addEventListener('input',function() {
-                    output.textContent = salary.value
-                });
 
-</script>
+var salary = document.querySelector("#salary");
+var output=  document.querySelector(".salary-output");
+output.textContent = salary.value;
+salary.addEventListener('input',function() {
+output.textContent = salary.value;
+ })
+
