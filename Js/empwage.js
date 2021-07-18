332fe2081b819af88dc33203e89f8ca270b1d562
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-07-18 22:12:18 +0530

    [akhila]Refcator:modify the app and adding new attributes

--- Js/empwage.js
+++ Js/empwage.js
@@ -7,33 +7,50 @@ output.textContent = salary.value;
  })
 //UC9-Adding getter setter method and validation to emppayroll app
 class EmployeePayrollData{
-    //property
-    id;
-    salary;
-    gender;
-    startdate;
-    //constructor
-   constructor(...params){
-       this.id = params[0];
-       this.name = params[1];
-       this.salary = params[2];
-       this.gender = params[3];
-       this.startdate = params[4];
-   }
-   //getter setter method
-   get name() { return this._name;}
+    //getter and setter method
+    get id() {return this._id; }
+    set id(id) {
+        this._id = id;
+    }
+    get name() { return this._name;}
    set name(name) {
-       let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+       let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
        if(nameRegex.test(name))
        this._name = name;
        else 
        throw 'Name is incorrect!';
    }
-   //method
-   toString()
-   const options = {year: 'numeric', month:'long' ,day='numeric' };
-   const empDate = !this.startDate ? "undefined" :
-                      this.startDate.toLocalDateString("en-US",options);
-    return "id=" +this.id+ ", name=" +this.name + ", salary= " + this.salary +","+
-         "gender=" +this.gender + ",startDate=" +empDate;
+    get profilepic() { return this._profilepic;} 
+    set profilepic(profilepic) {
+       this._profilepic = profilepic;
+    }
+    get gender() { return this._gender;}
+    set gender(gender) {
+        this._gender = gender;
+    }
+    get department() { return this._department;}
+    set department(department) {
+        this._department = department;
+    }
+    get salary() { return this._salary;}
+    set salary(salary){
+        this._salary = salary;
+    }
+    get note() { return this._note;}
+    set note(note){
+        this._note = note;
+    }
+    get satrtDate() {return this._sartDate; }
+    set startDate(startDate){
+        this._startDate = startDate;
+    }
+    //method
+    toString(){
+    const options = {year: 'numeric', month:'long' ,day='numeric' };
+    const empDate = !this.startDate ? "undefined" :
+                       this.startDate.toLocalDateString("en-US",options);
+     return "id=" +this.id+ ", name=" +this.name + ", salary= " + this.salary +","+
+          "gender=" +this.gender +",profilepic=" +this.profilepic +", department =" +this.department+
+           ",startDate=" +empDate+",note=" +this.note;
+    }
 }
