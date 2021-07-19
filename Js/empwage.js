2b765f83d22ecc5aa031ab5aec47f4dd92525f38
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-07-19 22:28:58 +0530

    [akhila]Refactor:saves the form data

--- Js/empwage.js
+++ Js/empwage.js
@@ -45,12 +45,37 @@ function validateDate() {
     textError.textContent = "";
   } else if (year.value > currentDate.getFullYear()) {
     setCurrentDate();
-  } else  (year.value == currentDate.getFullYear() && month.value > currentDate.getMonth()) 
+  } else if (year.value == currentDate.getFullYear() && month.value > currentDate.getMonth()) 
   {
     setCurrentDate();
-  } 
+  } else{
+    setCurrentDate();
+  }
 }
 year.addEventListener("input", validateDate);
 month.addEventListener("input", validateDate);
 day.addEventListener("input", validateDate);    
 });
+//setting gender with images default
+let filepath,genderValue;
+var profileImage = document.getElementsByName('profile');
+var gender = document.getElementsByName('gender');
+for(let index =0; index < profileImage.length; index++){
+  profileImage[index].addEventListener("click", function(){
+    filepath=profileImage[index].value;
+    if(profileImage[0].checked || profileImage[2].checked){
+      gender[0].checked = true;
+      genderValue = gender[0].value;
+    }if(profileImage[1].checked || profileImage[3].checked){
+      gender[1].checked = true;
+      genderValue = gender[1].value;
+    }
+    
+  });
+}
+//save the form
+var submit = document.querySelector("#submitbutton");
+submit.addEventListener("click", function(){
+alert(" "+ filepath +" "+ genderValue +""+ salary.value +""+ day.value  +" "+ month.value +" "+ year.value)
+});
+
